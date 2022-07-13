a44ef1f2a573b9289a34a952b4e47e48d2e543bd
gimhanul <gimhanuly@gmail.com>
2022-07-13 22:54:09 +0900

    init

---
+++ src/main/java/com/tlea/geonneo/GeonneoApplication.java
@@ -0,0 +1,13 @@
+package com.tlea.geonneo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeonneoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeonneoApplication.class, args);
+	}
+
+}
