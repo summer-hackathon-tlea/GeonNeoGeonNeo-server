1774912253ccf16c32d5f5a5985ef7752b24b8ee
gimhanul <gimhanuly@gmail.com>
2022-07-14 03:26:08 +0900

    FIX :: validation
    - 김한울... 한동안 프론트만 했더니 감 잃었다...

--- src/main/java/com/tlea/geonneo/domain/user/presentation/UserController.java
+++ src/main/java/com/tlea/geonneo/domain/user/presentation/UserController.java
@@ -5,8 +5,11 @@ import com.tlea.geonneo.domain.user.presentation.dto.response.TokenResponseDto;
 import com.tlea.geonneo.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
@@ -16,12 +19,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public void joinUser(UserRequestDto request) {
+    public void joinUser(@RequestBody @Valid UserRequestDto request) {
         userService.joinUser(request);
     }
 
     @PostMapping("/login")
-    public TokenResponseDto loginUser(UserRequestDto request) {
+    public TokenResponseDto loginUser(@RequestBody @Valid UserRequestDto request) {
         return userService.loginUser(request);
     }
 }
