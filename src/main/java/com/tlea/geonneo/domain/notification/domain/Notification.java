fb18d0ebb8cf598796f37492f84a43875d7fe96d
gimhanul <gimhanuly@gmail.com>
2022-07-14 09:33:51 +0900

    ADD :: sendDongho (#12)
    - send dongho field 추가

--- src/main/java/com/tlea/geonneo/domain/notification/domain/Notification.java
+++ src/main/java/com/tlea/geonneo/domain/notification/domain/Notification.java
@@ -3,23 +3,23 @@ package com.tlea.geonneo.domain.notification.domain;
 import com.tlea.geonneo.domain.user.domain.User;
 import com.tlea.geonneo.global.entity.BaseTimeEntity;
 import com.tlea.geonneo.global.utils.BooleanToYNConverter;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "notification_tbl")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@RequiredArgsConstructor
+@Getter
 public class Notification extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "notification_id")
     private Long id;
+
+    @Column(nullable = false)
+    private String sendDongho;
 
     @Column(nullable = false)
     private String content;
@@ -33,7 +33,8 @@ public class Notification extends BaseTimeEntity {
     private Boolean isRead;
 
     @Builder
-    public Notification(String content, User user) {
+    public Notification(String sendDongho, String content, User user) {
+        this.sendDongho = sendDongho;
         this.content = content;
         this.user = user;
         this.isRead = false;
