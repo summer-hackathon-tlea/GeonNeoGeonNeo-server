e4a473882a2b0aeef1b274dee39108ce5adcc27a
gimhanul <gimhanuly@gmail.com>
2022-07-14 10:33:19 +0900

    ADD :: validation method (#13)
    - authroity check method to facade
    - repository

--- src/main/java/com/tlea/geonneo/domain/with/domain/repository/WitherRepository.java
+++ src/main/java/com/tlea/geonneo/domain/with/domain/repository/WitherRepository.java
@@ -3,9 +3,11 @@ package com.tlea.geonneo.domain.with.domain.repository;
 import com.tlea.geonneo.domain.user.domain.User;
 import com.tlea.geonneo.domain.with.domain.With;
 import com.tlea.geonneo.domain.with.domain.Wither;
+import com.tlea.geonneo.domain.with.domain.type.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WitherRepository extends JpaRepository<Wither, Long> {
 
     boolean existsByWithAndUser(With with, User user);
+    boolean existsByWithAndUserAndAuthority(With with, User user, Authority authority);
 }
--- src/main/java/com/tlea/geonneo/domain/with/facade/WitherFacade.java
+++ src/main/java/com/tlea/geonneo/domain/with/facade/WitherFacade.java
@@ -3,7 +3,9 @@ package com.tlea.geonneo.domain.with.facade;
 import com.tlea.geonneo.domain.user.domain.User;
 import com.tlea.geonneo.domain.with.domain.With;
 import com.tlea.geonneo.domain.with.domain.repository.WitherRepository;
+import com.tlea.geonneo.domain.with.domain.type.Authority;
 import com.tlea.geonneo.domain.with.exception.WitherAlreadyExistsException;
+import com.tlea.geonneo.domain.with.exception.WitherDoesNotHavePermissionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +18,12 @@ public class WitherFacade {
     public void checkWither(With with, User user) {
         if (witherRepository.existsByWithAndUser(with, user)) {
             throw WitherAlreadyExistsException.EXCEPTION;
+        }
+    }
+
+    public void checkWitherPermission(With with, User user, Authority expectedAuthority) {
+        if (witherRepository.existsByWithAndUserAndAuthority(with, user, expectedAuthority)) {
+            throw WitherDoesNotHavePermissionException.EXCEPTION;
         }
     }
 }
