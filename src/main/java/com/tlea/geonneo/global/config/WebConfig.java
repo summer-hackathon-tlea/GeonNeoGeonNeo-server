eea2b475d7f2b6a165d8d6bae9da53ee0bda912b
gimhanul <gimhanuly@gmail.com>
2022-07-14 10:42:31 +0900

    FIX :: Cors (#14)

---
+++ src/main/java/com/tlea/geonneo/global/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.tlea.geonneo.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3002");
+    }
+}
